b986133bbb09d21158e7b8f44589ac714c27fc50
vikwilliamson <williamson.vik@gmail.com>
2019-10-03 00:32:05 -0400

    Added C# script to the collection, happy hacktober

---
+++ scripts/csharp_helloworld_vikwilliamson.cs
@@ -0,0 +1,17 @@
+// LANGUAGE: C#
+// AUTHOR: Vik Williamson
+// GITHUB: https://github.com/vikwilliamson
+
+using System;
+
+namespace HelloWorld 
+{
+    class Program 
+    {
+      static void Main(string[] args) 
+      {
+        Console.WriteLine("Hello World! Happy #Hacktoberfest!!!");
+        Console.ReadKey();
+      }
+    }
+}
